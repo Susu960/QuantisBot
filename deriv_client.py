488331143571f6322abb6eefa7c5a9bdc416f0d9
Susu960 <sulamitag887@gmail.com>
2026-05-16 15:20:04 +0100

    Update deriv_client.py

--- deriv_client.py
+++ deriv_client.py
@@ -1,25 +1,55 @@
 import json
 import websocket
 import logging
+import ssl
 
 logger = logging.getLogger("deriv-client")
 
+
 class DerivClient:
+
     def __init__(self, token):
+
         self.token = token
         self.ws = None
-        self.url = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
+
+        self.url = "wss://ws.derivws.com/websockets/v3?app_id=1089"
 
     def connect(self):
+
         try:
-            self.ws = websocket.create_connection(self.url)
-            self.ws.send(json.dumps({"authorize": self.token}))
+
+            self.ws = websocket.create_connection(
+                self.url,
+                sslopt={"cert_reqs": ssl.CERT_NONE}
+            )
+
+            auth_request = {
+                "authorize": self.token
+            }
+
+            self.ws.send(json.dumps(auth_request))
+
             response = json.loads(self.ws.recv())
-            return "error" not in response
-        except:
+
+            logger.info(f"Deriv auth response: {response}")
+
+            if "error" in response:
+
+                logger.error(response["error"])
+
+                return False
+
+            return True
+
+        except Exception as e:
+
+            logger.error(f"Connection error: {str(e)}")
+
             return False
 
     def buy(self, symbol, amount, contract_type):
+
         trade_data = {
             "buy": 1,
             "price": amount,
@@ -33,8 +63,16 @@ class DerivClient:
                 "symbol": symbol
             }
         }
+
         self.ws.send(json.dumps(trade_data))
-        return json.loads(self.ws.recv())
+
+        response = json.loads(self.ws.recv())
+
+        logger.info(f"Trade response: {response}")
+
+        return response
 
     def close(self):
-        if self.ws: self.ws.close()
+
+        if self.ws:
+            self.ws.close()
