32b3b4ae569c2cd0e71b269139fafc653d787ab7
Susu960 <sulamitag887@gmail.com>
2026-05-20 13:03:08 +0100

    Update deriv_client.py

--- deriv_client.py
+++ deriv_client.py
@@ -21,7 +21,7 @@ class DerivClient:
 
             headers = [
                 "Authorization: Bearer " + self.token,
-                "Deriv-App-ID: 33jnrgaYNKn4PLnXF7Z2k"
+                "Deriv-App-ID: 33jlLVvXXSH9iPFRywUjM"
             ]
 
             self.ws = websocket.create_connection(
